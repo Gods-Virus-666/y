0ac8c0ebdec8b94a2952112fd37d9115312be451
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-12-22 20:24:16 -0600

    Update youtube2mp4.py

--- youtube2mp4.py
+++ youtube2mp4.py
@@ -1,24 +1,10 @@
-#from pynput.mouse import Button, Controller
-#from pynput.keyboard import Key, Controller
-#mouse = Controller()
-#keyboard = Controller()
-#mouse.position = (123,704) #move to browser
-#mouse.click(Button.right, 1) #right click
-#mouse.position = (75, 500) #move to open new window
-#mouse.click(Button.left, 1) #clicks to open
-#keyboard.type("https://www.youtube.com/")
-#mouse.position=(40,100)
-#mouse.click(Button.left,1)
-#mouse.position=(100,425)
-#mouse.click(Button.left,1)
-
 #In order for this to work properly your screen resolution must be set at 1280x720.
 #The scale and layout set to 100%
 #Have only two tabs open. The first tab "https://www.youtube.com/playlist?list=WL" and the second "https://mp4s.org/en1/youtube-to-mp4/" 
 #If you attempt to use another mp4 site the program will not work.
 #Close all other applications and save any unsaved data. 
 #I would recommend setting the 'time.sleep(1)' to 'time.sleep(10)' if this is your first time running this program. 
-#The reason being you will have ten seconds before the next action takes place. Just in case the program malfunctions
+#That way you will have ten seconds before the next action takes place. Just in case the program malfunctions
 #I run this program with IDLE (Python 3.9 64-bit) the place where IDLE pops up is random so I usually move it 
 
 from pynput.mouse import Button, Controller
@@ -63,10 +49,10 @@ def youtube():
     mouse.position = (1219, 217) #Move to options button
     mouse.click(Button.left, 1) #click
     time.sleep(1)
-    mouse.position = (1103, 386) #Move to Remmove from playlist button
+    mouse.position = (1070, 420) #Move to Remove from playlist button
     mouse.click(Button.left, 1) #click
     time.sleep(1)
-for i in range (200, 0, -1): #Change the variable '200' to the amount of videos in the playlist.
+for i in range (200, 0, -1):  #Change the variable '200' to the amount of videos in the playlist.
     youtube()
-    time.sleep(180)
+    time.sleep(45) #Change the 45 to 180 if the video is longer than 5 minutes. 360 if it is an hour or longer
 print(dateTimeObj)
